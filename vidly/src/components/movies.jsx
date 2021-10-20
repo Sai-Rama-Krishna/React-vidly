840c5f41d29e6b60813d306e35df47c3a321b2f2
sai rama krishna <sairamakrishna2@gmail.com>
2021-10-20 19:47:46 +0530

    movies added

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+class Movies extends React.Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+
+    if (count === 0) return <p> There are no Movies</p>;
+
+    return (
+      <React.Fragment>
+        <div>
+          <p> Showing {count} movies in database </p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th> Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.movies.map((movie) => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td> {movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => this.handleDelete(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
