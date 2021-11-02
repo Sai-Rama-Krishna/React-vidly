a55837e2887d7e9cee692256b667da4d221c90d5
SaiRamaKrishna <sairamakrishna2@gmail.com>
2021-11-02 21:08:24 +0530

    final services

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,23 +4,30 @@ import http from "./httpService";
 
 const url = apiUrl + "/movies";
 
+function movieUrl(id) {
+  return `${url}/${id}`;
+  
+}
 export async function getMovies() {
-  return await  http.get(url);
+  return await http.get(url);
 }
 export function getMovie(movieId) {
-  return http.get(url + "/" + movieId );
+  
+
+  return http.get(movieUrl(movieId));
 }
 
-export  async function saveMovie(movie) {
+export async function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-     return  await http.put(url + "/" + movie._id, movie);
+
+    return await http.put(movieUrl(movie._id), body);
   }
 
-  return await  http.post(url, movie);
+  return await http.post(url, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(url + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
